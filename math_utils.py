42a78dd82e7abae9b3fc3fc4fb64352240440060
norsultani <norsultani@gmail.com>
2025-10-30 15:51:31 +0200

    Fix standard deviation calculation in find_mean_std

--- math_utils.py
+++ math_utils.py
@@ -19,6 +19,6 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = float((num1 + num2 + num3) / 3)
-    std = float(((((num1 - mean) ** 2) + ((num2 - mean) ** 2) + ((num2 - mean) ** 2)) / 3) ** 0.5)
+    std = float(((((num1 - mean) ** 2) + ((num2 - mean) ** 2) + ((num3 - mean) ** 2)) / 3) ** 0.5)
     return mean, std
 
